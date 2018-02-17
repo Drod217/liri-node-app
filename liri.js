9bceafd452dba3c873711449b1e3b5944131f423
drodriguez121m <drodriguez1219@yahoo.com>
2018-02-17 01:13:18 -0600

    Final Product with working selections..

--- liri.js
+++ liri.js
@@ -13,14 +13,27 @@ var access_token_secret = keys.twitterKeys.access_token_secret;
 
 var inquirer = require("inquirer");
 var twitter = require("twitter");
-var spotify = require("spotify");
+var Spotify = require("node-spotify-api");
 var request = require("request");
 var fs = require("fs");
 
-console.log("Spotify id: " + keys.spotifyKeys.id);
-console.log("Spotify secret: " + keys.spotifyKeys.secret);
+var spotify = new Spotify ({
+    id: keys.spotifyKeys.id,
+    secret: keys.spotifyKeys.secret
+});
+// var nodeArgs = process.argv;
 
-var itemName = "";
+// var itemName = "";
+
+// for(var i=3; i < nodeArgs.length; i++){
+//     if(i > 2 && i <nodeArgs.length){
+//         itemName= itemName + "+" + nodeArgs[i];
+//     }
+//     else{
+//         itemName += nodeArgs[i];
+//     }
+// }
+
 var source = "";
 var menuArray = ["my-tweets", "spotify-this-song", "movie-this", "do-what-it-says", "exit"];
 
@@ -94,7 +107,7 @@ function showTweets() {
     if (!error) {
 
       fs.appendFile("log.txt", "\n\nlog date: " + Date.now() + "\n", function(err) {
-        // If the code experiences any errors it will log the error to the console.
+        
         if (err) {
           return console.log(err);
         }
@@ -105,7 +118,7 @@ function showTweets() {
 
 
         fs.appendFile("log.txt", twitterOutput, function(err) {
-          // If the code experiences any errors it will log the error to the console.
+          
           if (err) {
             return console.log(err);
           }
@@ -119,7 +132,7 @@ function showTweets() {
 
 
 function spotifyInput() {
-  source = "spotify";
+  source = "node-spotify-api";
   console.log(""); 
   console.log(""); 
   inquirer.prompt([
@@ -127,104 +140,77 @@ function spotifyInput() {
       type: "input",
       name: "song",
       message: "Enter a song name",
-      default: "99 Problems"
+      default: "99 problems"
     }
   ]).then(function(spotifyUser) {
     spotifyResults(spotifyUser.song)
   });
 };
 
-function spotSong(){
-    if(!itemName){
-        itemName= "The "+"Sign";
-    }
-    spotify.search({ type: 'track', query: itemName }, function(err, data) {
-       console.log("Hi " + itemName);
+
+
+
+function spotifyResults(song) {
+      console.log(song);
+      
+      spotify.search({type: 'track', query: song}, function(err, data) {
+      if(err) {
+        return console.log("Error occurred: " + err);
+        
+      }
+      var results = data.tracks.items.length;
+      fs.appendFile("log.txt", "\n\nlog date: " + Date.now() + "\n", function(err) {
+        
         if (err) {
-            console.log('Error occurred: ' + err);
-        } else {
-            if (!data.tracks.items.length) {
-                console.log('not found')
-            }
-            for(var i=0; i < data.tracks.items.length; i++){
-                console.log("--------------------------------------------");
-                console.log("Track Name: " + data.tracks.items[i].name); 
-                console.log("Artist: " + data.tracks.items[i].artists[0].name); 
-                console.log("Album: " + data.tracks.items[i].album.name); 
-                console.log("Preview Link: " + data.tracks.items[i].href); 6 
-            }
+          return console.log(err);
         }
-    });
-}
-
-
-
-
-
-
-
-
-// function spotifyResults(song) {
-//   console.log(song);
-//   spotify.search({type: 'track', query: song}, function(err, data) {
-//     if(err) {
-//       console.log("Error occurred: " + err);
-//       return;
-//     };
-    
-//     var results = data.tracks.items.length;
-//     fs.appendFile("log.txt", "\n\nlog date: " + Date.now() + "\n", function(err) {
-//       // If the code experiences any errors it will log the error to the console.
-//       if (err) {
-//         return console.log(err);
-//       }
-//     });  
-//     for (var i=-0; i<results; i++) {
-//       var spotifyOutput = "\n\nartist: " + data.tracks.items[i].album.artists[0].name +
-//       "\nalbum: " + data.tracks.items[i].album.name +
-//       "\ntrack: " + console.log("track: " + data.tracks.items[i].name + 
-//       "\npreview: " + data.tracks.items[i].preview_url);
-//       console.log(spotifyOutput);
-//       fs.appendFile("log.txt", spotifyOutput, function(err) {
-//         // If the code experiences any errors it will log the error to the console.
-//         if (err) {
-//           return console.log(err);
-//         }
-//       });   
-//     };
-//     if (source === "do-this") {
-//       mainMenu();
-//     } else {
-//     spotifyMenu();
-//     };
-//   });
-// };
-
-
-
-function spotifyMenu(){
-  console.log(""); 
-  console.log(""); 
-  inquirer.prompt([
-    {
-      type: "list",
-      name: "spotifySelection",
-      message: "Make a selection:",
-      choices: ["Pick a New Song", "Main Menu", "Exit"]
-    }
-  ]).then(function(spotifyMenuChoice) {
-    switch(spotifyMenuChoice.spotifySelection) {
-      case "Pick a New Song":
-        spotifyInput();
-        break;
-      case "Main Menu":
+      });  
+      for (var i=-0; i<results; i++) {
+        var spotifyOutput = "\n\nartist: " + data.tracks.items[i].album.artists[0].name +
+        "\nalbum: " + data.tracks.items[i].album.name +
+        "\ntrack: " + console.log("track: " + data.tracks.items[i].name + 
+        "\npreview: " + data.tracks.items[i].preview_url);
+        console.log(spotifyOutput);
+        fs.appendFile("log.txt", spotifyOutput, function(err) {
+          // If the code experiences any errors it will log the error to the console.
+          if (err) {
+            return console.log(err);
+          }
+        });   
+      }
+      if (source === "do-this") {
         mainMenu();
-        break;
-      case "Exit":
-        break;
-    };
-  });
-};
+      } else {
+      spotifyMenu();
+      }
+    })
+  }
+  
+  
+  
+  function spotifyMenu(){
+    console.log(""); 
+    console.log(""); 
+    inquirer.prompt([
+      {
+        type: "list",
+        name: "spotifySelection",
+        message: "Make a selection:",
+        choices: ["Pick a New Song", "Main Menu", "Exit"]
+      }
+    ]).then(function(spotifyMenuChoice) {
+      switch(spotifyMenuChoice.spotifySelection) {
+        case "Pick a New Song":
+          spotifyInput();
+          break;
+        case "Main Menu":
+          mainMenu();
+          break;
+        case "Exit":
+          break;
+      };
+    });
+  };
 
 
 
